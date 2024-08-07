6e682a8f3bed3ca7b77b4224db5c964bdb30dc9f
Wesley Sibanyoni <Wesley.sibanyoni@gmail.com>
2024-08-07 18:03:32 +0200

    "Removed Navbar component and its stylesheet, added Navbar import to App component"

--- Wes-port/src/App.jsx
+++ Wes-port/src/App.jsx
@@ -1,8 +1,9 @@
 import "./app.scss"
+import Navbar from "./Components/Navbar/Navbar";
 
 const App = () => {
   return <div>
-    <section><a href="" className="Head">Head</a></section>
+    <section><a href="" className="Head">Head</a> <Navbar></Navbar></section>
     <section>Hero</section>
     <section>Features</section>
     <section>Call to Action</section>
