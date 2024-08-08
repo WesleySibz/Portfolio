97a5f4c9c9f51a19ac788b63f9223f8f73951d05
Wesley Sibanyoni <Wesley.sibanyoni@gmail.com>
2024-08-08 16:25:13 +0200

    "Updated package-lock.json and package.json to include prop-types, removed 'dev' flag from object-assign and prop-types, renamed TogBtn to ToggleButton, and deleted TogBtn.jsx file."

---
+++ Wes-port/src/Components/Sidebar/ToggleButton/ToggleButton.jsx
@@ -0,0 +1,13 @@
+import PropTypes from 'prop-types';
+
+const ToggleButton = ({ setOpen }) => {
+  return (
+    <button onClick={() => setOpen(prev => !prev)}>Button</button>
+  );
+}
+
+ToggleButton.propTypes = {
+  setOpen: PropTypes.func.isRequired,
+};
+
+export default ToggleButton;
