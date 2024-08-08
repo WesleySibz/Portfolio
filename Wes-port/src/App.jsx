e3eea3567cbf81440db7651f7879cc8dc27f94c6
Wesley Sibanyoni <Wesley.sibanyoni@gmail.com>
2024-08-08 14:31:17 +0200

    "Updated App.jsx, Navbar.jsx, navbar.scss, and app.scss files: toggled import statements, added framer-motion to Navbar, modified styles and layout."

--- Wes-port/src/App.jsx
+++ Wes-port/src/App.jsx
@@ -1,20 +1,21 @@
 import "./app.scss"
-//import Navbar from "./Components/Navbar/Navbar";
-import Test from "../src/Test";
+import Navbar from "./Components/Navbar/Navbar";
+//import Test from "../src/Test";
 
 const App = () => {
   return <div>
-{/* {    <section><Navbar/></section>
+    <section><Navbar/></section>
     <section>Hero</section>
     <section>Features</section>
     <section>Call to Action</section>
     <section>Projects</section>
     <section>About</section>
     <section>Contact</section>
-    <section>footer</section>} */}
+    <section>footer</section>
+
+    {/* {<Test/>
     <Test/>
-    <Test/>
-    <Test/>
+    <Test/>} */}
 
   </div>;
 }
--- Wes-port/src/Components/Navbar/Navbar.jsx
+++ Wes-port/src/Components/Navbar/Navbar.jsx
@@ -1,3 +1,4 @@
+import {motion} from "framer-motion"
 import "./navbar.scss"
 
 const Navbar = () => {
@@ -5,7 +6,11 @@ const Navbar = () => {
     <div className="navbar">
       <div className="wrapper">
 
-        <span>Wesley Sibanyoni</span>
+        <motion.span 
+        initial={{opacity:0, scale:0.5}}
+        animate={{opacity:1, scale:0.5}}
+        transition={{duration:0.5}}
+        >Wesley Sibanyoni</motion.span>
 
         <div className="social">
           <a href="Dribble"><img src=".\src\Components\assets\dribble.png" alt="Dribble" /></a>
