e0fbcd7515d9d2c276b6c08535b9ba35792583e4
Wesley Sibanyoni <Wesley.sibanyoni@gmail.com>
2024-08-09 11:15:16 +0200

    "Updated ToggleButton component to use Framer Motion and modified sidebar styles"

--- Wes-port/src/Components/Sidebar/ToggleButton/ToggleButton.jsx
+++ Wes-port/src/Components/Sidebar/ToggleButton/ToggleButton.jsx
@@ -1,13 +1,43 @@
 import PropTypes from 'prop-types';
+import {motion} from "framer-motion";
 
 const ToggleButton = ({ setOpen }) => {
   return (
     <button onClick={() => setOpen(prev => !prev)}>
-      <svg width="30" height="30" viewBox="0 0 24 24">
-        <path d="M2 4 H21" strokeWidth="4" stroke="black" strokeLinecap="round"/>
-        <path d="M2 12 H21" strokeWidth="4" stroke="black" strokeLinecap="round"/>
-        <path d="M2 20 H21" strokeWidth="4" stroke="black" strokeLinecap="round"/>
+
+      <svg width="25" height="25" viewBox="0 0 23 23">
+
+        <motion.path
+          strokeWidth="4" 
+          stroke="black" 
+          strokeLinecap="round" 
+          variants={{
+            closed:{d:"M 2 2 L 20 2"},
+            open:{d:"M 3 16.5 L 17 2.5"},
+          }}
+        />
+
+        <motion.path 
+          strokeWidth="4" 
+          stroke="black" 
+          strokeLinecap="round" 
+          d="M 2 10 L 20 10"
+          variants={{
+            closed:{opacity:"1"},
+            open:{opacity:"0"},
+            }}
+          />
+        <motion.path
+          strokeWidth="4" 
+          stroke="black" 
+          strokeLinecap="round" 
+          variants={{
+            closed:{d:"M 2 18 L 20 18"},
+            open:{d:"M 3 2.5 L 17 16.346"},
+          }}
+        />
       </svg>
+
     </button>
   );
 }
