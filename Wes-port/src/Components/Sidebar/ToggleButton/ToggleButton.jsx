c4404fa0f14ebf9c1ef27547f368e0ebc6bfd113
Wesley Sibanyoni <Wesley.sibanyoni@gmail.com>
2024-08-09 10:48:06 +0200

    "Updated ToggleButton component to use SVG icon instead of text, and adjusted CSS styles for button position and size."

--- Wes-port/src/Components/Sidebar/ToggleButton/ToggleButton.jsx
+++ Wes-port/src/Components/Sidebar/ToggleButton/ToggleButton.jsx
@@ -2,7 +2,13 @@ import PropTypes from 'prop-types';
 
 const ToggleButton = ({ setOpen }) => {
   return (
-    <button onClick={() => setOpen(prev => !prev)}>Button</button>
+    <button onClick={() => setOpen(prev => !prev)}>
+      <svg width="30" height="30" viewBox="0 0 24 24">
+        <path d="M2 4 H21" strokeWidth="4" stroke="black" strokeLinecap="round"/>
+        <path d="M2 12 H21" strokeWidth="4" stroke="black" strokeLinecap="round"/>
+        <path d="M2 20 H21" strokeWidth="4" stroke="black" strokeLinecap="round"/>
+      </svg>
+    </button>
   );
 }
 
