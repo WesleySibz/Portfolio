f77c1e9537518f330d7b184c7c8813807248aee4
Wesley Sibanyoni <Wesley.sibanyoni@gmail.com>
2024-08-14 11:52:03 +0200

    "Added Framer Motion animations to Sidebar and Links components"

--- Wes-port/src/Components/Sidebar/Links/Links.jsx
+++ Wes-port/src/Components/Sidebar/Links/Links.jsx
@@ -1,15 +1,44 @@
+import {motion} from 'framer-motion';
 
+
+const variants = {
+  open:{
+  transition: {
+    staggerChildren:1,
+    staggerDirection:1
+  },
+},
+  closed:{
+  transition: {
+    staggerChildren:0.05,
+    staggerDirection:-1,
+  },
+},
+};
+
+const itemVariants = {
+  open:{
+  y: 0,
+  opacity:1,
+  transition:{duration:1, delay:0.4},
+},
+  closed:{
+  y:150,
+  opacity:0,
+  transition:{duration:0.2},
+},
+};
 
 const Links = () => {
 
     const items =["HomePage","Projects","About","Contact"];
 
   return (
-    <div className="links">
+    <motion.div className="links" variants={variants}>
         {items.map(item=>(
-            <a href={`#${item}`} key={item}>{item}</a>
+            <motion.a href={`#${item}`} key={item} variants={itemVariants}>{item}</motion.a>
         ))}
-    </div>
+    </motion.div>
   )
 }
 
--- Wes-port/src/Components/Sidebar/Sidebar.jsx
+++ Wes-port/src/Components/Sidebar/Sidebar.jsx
@@ -4,30 +4,31 @@ import {motion} from "framer-motion"
 import "./sidebar.scss"
 import ToggleButton from "./ToggleButton/ToggleButton"
 //import { type } from "os"
+
+const variants = {
+    open:{
+        clipPath: "circle(1200px at 50px 50px)",
+        transition:{
+        delay:0,
+        type: "spring",
+        stiffness: 20,
+        damping: 5,
+    }
+},
+    closed:{
+        clipPath:"circle(30px at 50px 50px)",
+        transition:{
+        delay:0,
+        type: "spring",
+        stiffness: 200,
+        damping: 60,
+        }
+    }
+} 
+
 const Sidebar = () => {
 
     const [open,setOpen] = useState(false)
-
-    const variants = {
-        open:{
-            clipPath: "circle(1200px at 50px 50px)",
-            transition:{
-            delay:0,
-            type: "spring",
-            stiffness: 20,
-            damping: 5,
-        }
-    },
-        closed:{
-            clipPath:"circle(30px at 50px 50px)",
-            transition:{
-            delay:0,
-            type: "spring",
-            stiffness: 400,
-            damping: 60,
-            }
-        }
-    } 
 
   return (
     <motion.div className="sidebar" animate={open ?"open" : "closed"}>
