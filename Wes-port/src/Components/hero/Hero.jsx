bc4954b192c58f5cb88055552ccc48013c9d0dc7
Wesley Sibanyoni <Wesley.sibanyoni@gmail.com>
2024-08-14 14:28:07 +0200

    "Added Framer Motion animations to Hero component and updated hero.scss with new styles for sliding text container"

--- Wes-port/src/Components/hero/Hero.jsx
+++ Wes-port/src/Components/hero/Hero.jsx
@@ -1,29 +1,56 @@
 import './hero.scss';
 import {motion} from 'framer-motion';
 
+
+const textVariants={
+    initial: {
+        x:-500,
+        opacity: 0,
+    },
+    animate: {
+        x:0,
+        opacity: 1,
+        transistion:{
+            duration: 2,
+            staggerChildren: 0.1,
+        }
+    },
+}
+
 const Hero = () => {
   return (
     <div className='hero'>
         <div className="wrapper">
 
-        <div className='text-container'>
-            <h2>Wesley Shane</h2>
-            <h1> Don't nothing come to a sleeper but a dream</h1>
+        <motion.div 
+        className='text-container' 
+        variants={textVariants} 
+        initial="initial" 
+        animate="animate">
 
-            <div className="buttons">
+            <motion.h2 variants={textVariants}>Wesley Shane</motion.h2>
+            <motion.h1 variants={textVariants}> Dont nothing come to a sleeper but a dream</motion.h1>
+
+            <motion.div className="buttons" variants={textVariants}>
             <motion.button 
             whileHover={{scale: 1.1}}
-            whileTap={{scale: 0.95}}>
+            whileTap={{scale: 0.95}}
+            variants={textVariants}>
                 See Latest Project
             </motion.button>
             <motion.button
             whileHover={{scale: 1.1}}
-            whileTap={{scale: 0.95}}>
+            whileTap={{scale: 0.95}}
+            variants={textVariants}>
                 Contact Me
             </motion.button>
+            </motion.div>
+            <motion.img variants={textVariants} src=".\src\Components\assets\linkedin.png" alt="" />
+
+            <div className='slindingTxtContainer'>
+                Coder Editor Trader Influencer 
             </div>
-            <img src=".\src\Components\assets\linkedin.png" alt="" />
-        </div>
+        </motion.div>
         
         <div className='img-container'>
             <img src='.\src\Components\assets\cvphoto.png' alt=''/>
