67d735509f7120e285b09b1a490d0b03fd225b88
Wesley Sibanyoni <Wesley.sibanyoni@gmail.com>
2024-08-14 13:42:03 +0200

    "Updated Hero component with new HTML structure and styles"

--- Wes-port/src/Components/hero/Hero.jsx
+++ Wes-port/src/Components/hero/Hero.jsx
@@ -3,9 +3,25 @@ import './hero.scss'
 const Hero = () => {
   return (
     <div className='hero'>
+        <div className="wrapper">
+
+        <div className='text-container'>
+            <h2>Wesley Shane</h2>
+            <h1>Don't nothing come to a sleeper but a dream</h1>
+
+            <div className="buttons">
+            <button>See Latest Project</button>
+            <button>Contact Me</button>
+            </div>
+            <img src=".\src\Components\assets\linkedin.png" alt="" />
+        </div>
+        
         <div className='img-container'>
             <img src='.\src\Components\assets\cvphoto.png' alt=''/>
         </div>
+        </div>
+
+        
     </div>
   )
 }
