21cf36b9d0229a0ffdc2a67f7f66f1198b2d2b5a
Wesley Sibanyoni <Wesley.sibanyoni@gmail.com>
2024-08-14 11:56:33 +0200

    "Updated Links component with hover and tap effects, added new Hero component"

--- Wes-port/src/Components/Sidebar/Links/Links.jsx
+++ Wes-port/src/Components/Sidebar/Links/Links.jsx
@@ -36,8 +36,16 @@ const Links = () => {
   return (
     <motion.div className="links" variants={variants}>
         {items.map(item=>(
-            <motion.a href={`#${item}`} key={item} variants={itemVariants}>{item}</motion.a>
-        ))}
+            <motion.a 
+            href={`#${item}`} 
+            key={item} 
+            variants={itemVariants} 
+            whileHover={{scale: 1.1}}
+            whileTap={{scale: 0.95}}
+            >
+              {item}
+            </motion.a>
+          ))}
     </motion.div>
   )
 }
