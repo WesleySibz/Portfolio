5311c8aee4347967c63d405695739c61a3f70f9c
Wesley Sibanyoni <Wesley.sibanyoni@gmail.com>
2024-08-16 10:25:54 +0200

    "Updated Navbar component to animate Dribble image and refactored hero component styles"

--- Wes-port/src/Components/Navbar/Navbar.jsx
+++ Wes-port/src/Components/Navbar/Navbar.jsx
@@ -15,7 +15,10 @@ const Navbar = () => {
         >Wesley Sibanyoni</motion.span>
 
         <div className="social">
-          <a href="Dribble"><img src=".\src\Components\assets\dribble.png" alt="Dribble" /></a>
+          <a href="Dribble"><motion.img 
+          animate={{opacity:1, duration:0.5}}
+          transition={{duration:0.5}}
+          src=".\src\Components\assets\dribble.png" alt="Dribble" /></a>
           <a href="Github"><img src=".\src\Components\assets\github.png" alt="Github" /></a>
           <a href="LinkedIn"><img src=".\src\Components\assets\linkedin.png" alt="LinkedIn" /></a>
         </div>
